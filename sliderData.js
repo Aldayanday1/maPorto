113e0ef10c5bb8337bea28e927a6d4452c5e2ffd
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2025-09-07 13:24:04 +0700

    parallax try

---
+++ sliderData.js
@@ -0,0 +1,6 @@
+export const sliderData = [
+  { img: "images/build-1.jpg", title: "Project Build 1", url: "#" },
+  { img: "images/build-2.jpg", title: "Project Build 2", url: "#" },
+  { img: "images/build-3.jpg", title: "Project Build 3", url: "#" },
+  { img: "images/build-4.jpg", title: "Project Build 4", url: "#" },
+];
